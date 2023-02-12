338bc9463d58d26d7684d33355adffc7282aca59
oxcafedead <a.vozhdaenko@gmail.com>
2023-02-12 16:16:20 +0200

    v0.0.3
    fixed couple of connection issues
    
    if controller is disconnected, no need to relaunch the converter app.

--- build.bat
+++ build.bat
@@ -1,13 +1,16 @@
+rmdir /s /q build
 javac -d build -cp "lib/*;" .\src\oxcafedead\g2k\*
 cd build
 jar cmf ..\Manifest.MF g2k.jar oxcafedead\g2k\*
 jlink --output jre --compress=2 --no-header-files --no-man-pages --add-modules java.logging --add-modules java.desktop
-cp -r ..\lib lib
-cp ..\icon.png icon.png
-cp ..\default.g2k default.g2k
+xcopy /s /e /i ..\lib .\lib
+copy ..\icon.png icon.png
+copy ..\default.g2k default.g2k
 echo java -Djava.library.path=lib -jar g2k.jar >> startup.bat
 
 launch4jc.exe ..\exe-conf.xml
 
 7z a -tzip gamepad2keyboard.zip jre lib app.exe default.g2k g2k.jar icon.png
 7z a -tzip gamepad2keyboard-nojava.zip lib default.g2k g2k.jar icon.png startup.bat
+
+cd ..
--- src/oxcafedead/g2k/Main.java
+++ src/oxcafedead/g2k/Main.java
@@ -19,96 +19,93 @@ import net.java.games.input.EventQueue;
 
 public class Main {
 
-	private static boolean debug = false;
-	private static ConfigWindow configWindow;
+    private static boolean debug = false;
+    private static ConfigWindow configWindow;
 
-	public static void main(String[] args) throws AWTException, InterruptedException {
+    public static void main(String[] args) throws AWTException, InterruptedException {
 
-		if (Arrays.stream(args).anyMatch(s -> s.equalsIgnoreCase("debug"))) {
-			debug = true;
-		}
+        if (Arrays.stream(args).anyMatch(s -> s.equalsIgnoreCase("debug"))) {
+            debug = true;
+        }
 
-		var selectControllerWindow = new SelectControllerWindow();
-		var controller = selectControllerWindow.getController();
-		selectControllerWindow.close();
-		configWindow = new ConfigWindow(controller);
+        var selectControllerWindow = new SelectControllerWindow();
+        var controller = selectControllerWindow.getController();
+        selectControllerWindow.close();
+        configWindow = new ConfigWindow(controller);
 
-		Robot robot = new Robot();
+        Robot robot = new Robot();
 
-		Executors.newSingleThreadExecutor().submit(() -> {
-			while (true) {
-				if (!controller.poll()) {
-					JOptionPane.showMessageDialog(null, "Could not connect to selected device!" +
-							"\nRestart the program, check the connection or try to select another one.");
-					System.exit(1);
-				}
+        Executors.newSingleThreadExecutor().submit(() -> {
+            while (true) {
+                if (!controller.poll()) {
+                    System.err.println("Could not connect to selected device!");
+                    Thread.sleep(100);
+                }
 
-				Thread.sleep(20);
-				EventQueue queue = controller.getEventQueue();
+                EventQueue queue = controller.getEventQueue();
 
-				Event event = new Event();
+                Event event = new Event();
 
-				if (!queue.getNextEvent(event)) {
-					continue;
-				}
-				Component comp = event.getComponent();
+                if (!queue.getNextEvent(event)) {
+                    Thread.sleep(20);
+                    continue;
+                }
+                Component comp = event.getComponent();
 
-				debug("event received from device: " + event + " ID === " + comp.getIdentifier());
+                debug("event received from device: " + event + " ID === " + comp.getIdentifier());
 
-				Util.addEvent(event);
-			}
-		});
+                Util.addEvent(event);
+            }
+        });
 
-		while (true) {
-			if (Thread.interrupted()) {
-				info("Program is shutting down...");
-				return;
-			}
+        while (true) {
+            if (Thread.interrupted()) {
+                info("Program is shutting down...");
+                return;
+            }
 
-			var event = Util.nextEvent();
-			var bindingsProfile = configWindow.bindingsProfile();
-			Collection<Integer> keys = bindingsProfile.getCodes(event);
-			keys.forEach(key -> {
-				bindingsProfile.getCodesToRelease(event).forEach(robot::keyRelease);
+            var event = Util.nextEvent();
+            var bindingsProfile = configWindow.bindingsProfile();
+            Collection<Integer> keys = bindingsProfile.getCodes(event);
+            keys.forEach(key -> {
+                bindingsProfile.getCodesToRelease(event).forEach(robot::keyRelease);
 
-				if (key == -1) {
-					return;
-				}
+                if (key == -1) {
+                    return;
+                }
 
-				var identifier = event.getComponent().getIdentifier();
-				var isPov = identifier.equals(Component.Identifier.Axis.POV);
-				if (isPov && Math.abs(event.getValue()) == 0
-						|| !isPov && Math.abs(event.getValue()) < bindingsProfile.analogSensitivity() / 100f) {
-					debug("reacting: release " + KeyEvent.getKeyText(key));
-					robot.keyRelease(key);
-				}
-				else {
-					if (!event.getComponent().isAnalog() || Math.abs(event.getValue()) >= bindingsProfile.analogSensitivity() / 100f) {
-						debug("reacting: pressing " + KeyEvent.getKeyText(key));
-						robot.keyPress(key);
-					}
-				}
-			});
-		}
+                var identifier = event.getComponent().getIdentifier();
+                var isPov = identifier.equals(Component.Identifier.Axis.POV);
+                if (isPov && Math.abs(event.getValue()) == 0
+                        || !isPov && Math.abs(event.getValue()) < bindingsProfile.analogSensitivity() / 100f) {
+                    debug("reacting: release " + KeyEvent.getKeyText(key));
+                    robot.keyRelease(key);
+                } else {
+                    if (!event.getComponent().isAnalog() || Math.abs(event.getValue()) >= bindingsProfile.analogSensitivity() / 100f) {
+                        debug("reacting: pressing " + KeyEvent.getKeyText(key));
+                        robot.keyPress(key);
+                    }
+                }
+            });
+        }
 
-	}
+    }
 
-	static void debug(String info) {
-		if (!debug) return;
-		System.out.println("[DEBUG]\t" + info);
-	}
+    static void debug(String info) {
+        if (!debug) return;
+        System.out.println("[DEBUG]\t" + info);
+    }
 
-	static void info(String info) {
-		System.out.println("[INFO]\t" + info);
-	}
+    static void info(String info) {
+        System.out.println("[INFO]\t" + info);
+    }
 
-	static Image icon() {
-		try {
-			return new ImageIcon(Path.of("icon.png").toUri().toURL()).getImage();
-		}
-		catch (MalformedURLException e) {
-			throw new IllegalStateException("no icon");
-		}
-	}
+    static Image icon() {
+        try {
+            return new ImageIcon(Path.of("icon.png").toUri().toURL()).getImage();
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException("no icon");
+        }
+    }
 
 }
